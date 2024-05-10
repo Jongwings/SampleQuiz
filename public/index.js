d369e120527517221e8b988a3419a9e8298a5f59
Jong Wings <jongwings@Jongs-MacBook-Pro.local>
2024-05-10 16:08:12 +0530

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var HeaderTitle = document.getElementById('HeaderTitle');
+    HeaderTitle.textContent = 'Tamil Quiz';
+
+    var HeaderInfo = document.getElementById('HeaderInfo');
+    HeaderInfo.textContent = 'Welcome to our Tamil Quiz application';
+
+});
+
