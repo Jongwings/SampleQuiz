22b3512c05d66e7cc4046828cb4282c85dfebc17
Jong Wings <jongwings@Jongs-MacBook-Pro.local>
2024-06-17 17:43:33 +0530

    Organize project for GitHub Pages

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '')));
 
 // Start the server
 const PORT = process.env.PORT || 3000;
